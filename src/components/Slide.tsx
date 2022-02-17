e3328eaa6166092cbefdd853276f990e318dd1de
alvaro fragoso <alvarofragosoc@hotmail.com>
2022-02-17 18:42:36 +0100

    Fixed Carousel

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,55 +1,49 @@
-import React from "react";
+import React, { Component } from 'react';
 import Image from "next/dist/client/image";
 import futbolista from "/public/futbolista.png";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 export const Slide = () => {
   return (
-    <div>
-    <div className="relative flex gap-6 snap-x overflow-x-auto pb-14 no-bar">
-    <div className="snap-center shrink-0">
-      <div className="shrink-0 w-4 sm:w-48"></div>
-    </div>
-      <div className="snap-center flex-shrink-0 first:pl-8 last:pr-8 w-2/6 bg-green-100 rounded-md">
-        <Image
-          className="flex-shrink-0 w-60 rounded-lg shadow-xl "
-          src={futbolista}
-          alt="culo"
-        />
-      </div>
-      <div className="snap-center flex-shrink-0 first:pl-8 last:pr-8 w-2/6 bg-blue-100 rounded-md">
-        <Image
-          className="flex-shrink-0 w-60  rounded-lg shadow-xl "
-          src={futbolista}
-          alt="culo"
-        />
-      </div>
-      <div className="snap-center flex-shrink-0 first:pl-8 last:pr-8 w-2/6 bg-yellow-100 rounded-md">
-        <Image
-          className="flex-shrink-0 w-60 rounded-lg shadow-xl "
-          src={futbolista}
-          alt="culo"
-        />
-      </div>
-      <div className="snap-center flex-shrink-0 first:pl-8 last:pr-8 w-2/6 bg-red-100 rounded-md ">
-        <Image
-          className="flex-shrink-0 w-60  rounded-lg shadow-xl"
-          src={futbolista}
-          alt="culo"
-        />
-      </div>
-      <div className="snap-center flex-shrink-0 first:pl-8 last:pr-8 w-2/6 bg-purple-100 rounded-md">
-        <Image
-          className="flex-shrink-0 w-60  rounded-lg shadow-xl "
-          src={futbolista}
-          alt="culo"
-        />
-      </div>
-      <div className="snap-center shrink-0">
-      <div className="shrink-0 w-4 sm:w-48"></div>
-    </div>
+    <Carousel autoPlay={true}  infiniteLoop={true} centerMode={true} centerSlidePercentage={25} emulateTouch={true} showStatus={false} showArrows={false}>
 
-    </div>
+      <div className=" w-5/6 bg-green-100 rounded-md">
+        <Image
+          className="  rounded-lg shadow-xl "
+          src={futbolista}
+          alt="culo"
+        />
+      </div>
+      <div className="w-5/6 bg-blue-100 rounded-md">
+        <Image
+          className="  rounded-lg shadow-xl "
+          src={futbolista}
+          alt="culo"
+        />
+      </div>
+      <div className="w-5/6   bg-yellow-100 rounded-md">
+        <Image
+          className=" rounded-lg shadow-xl "
+          src={futbolista}
+          alt="culo"
+        />
+      </div>
+      <div className="w-5/6   bg-red-100 rounded-md ">
+        <Image
+          className="   rounded-lg shadow-xl"
+          src={futbolista}
+          alt="culo"
+        />
+      </div>
+      <div className="w-5/6  bg-purple-100 rounded-md">
+        <Image
+          className="   rounded-lg shadow-xl "
+          src={futbolista}
+          alt="culo"
+        />
+      </div>
 
-    </div>
+    </Carousel>
   );
+
 };
